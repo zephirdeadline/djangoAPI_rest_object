17a49afc5876f1b63d065223030fdfb7d7081bce
w4pity <melvyn.petrocchi@gmail.com>
2018-09-23 16:17:15 +0200

    add circleci

--- rest_object/tests.py
+++ rest_object/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class CreateAccount(TestCase):
+
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        self.assertEqual(1,1)
